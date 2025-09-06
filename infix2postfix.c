bb683abb0bfafb5d2f816e825ab311706ce6e443
Athul George <athulgeorge2028@cs.sjcetpalai.ac.in>
2025-09-06 16:04:57 +0530

    Update infix2postfix.c

--- infix2postfix.c
+++ infix2postfix.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 100
+int precedence(char op) 
+{
+    if(op=='^') return 3;
+    if(op=='*'||op=='/') return 2;
+    if(op=='+'||op=='-') return 1;
+    return 0;
+}
+int isRightAssociative(char op)
+{
+    return (op=='^');
+}
+int isOperand(char ch) 
+{
+    return((ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z')||(ch>='0'&&ch<='9'));
+}
 int main()
 {
     char infix[MAX],postfix[MAX],stack[MAX];
     int i=0,j=0,top=-1;
     char ch;
-    printf("Enter the Expressiom(no whitespace):");
+    printf("Enter the Expression(no whitespace):");
     scanf("%s",infix);
     while(infix[i]!='\0')
     {
         ch=infix[i];
-        if((ch>='A'&& ch<='Z')||(ch>='a' && ch<='z')||(ch>='0'&&ch<='9'))
+        if(isOperand(ch))
         {
             postfix[j++]=ch;
         }
-        else if(ch=='(')
+        else if(ch=='(') 
         {
             stack[++top]=ch;
         }
-        else if(ch==')')
+        else if(ch==')') 
         {
-            while(top!=-1 && stack[top]!='(')
-                {
-                    postfix[j++]=stack[top--];
-                }
-                if(top!=-1)
-                {
-                    top--;
-                }
+            while(top!=-1&&stack[top]!='(') 
+            {
+                postfix[j++]=stack[top--];
+            }
+            if(top!=-1&&stack[top]=='(') top--;
         }
         else 
         {
-            while (top !=-1 && ((stack[top]=='^'&&ch != '^') || (stack[top]=='*'||stack[top]=='/')
-                    || (stack[top]=='+'||stack[top]=='-')&&(ch=='+'||ch=='-'))) 
-                {
-                postfix[j++] = stack[top--];
-                }
+            while(top!=-1&&stack[top]!='('&&
+            (precedence(stack[top])>precedence(ch)||
+            (precedence(stack[top])==precedence(ch)&&!isRightAssociative(stack[top])))) 
+            {
+                postfix[j++]=stack[top--];
+            }
             stack[++top]=ch;
         }
         i++;
@@ -46,5 +60,5 @@ int main()
     }
     postfix[j]='\0';
     printf("Postfix Expression: %s\n",postfix);
-return 0;
+    return 0;
 }
